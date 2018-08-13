313678441beb5e452e3f4bfa423106fb924095a5
pippo-tantan <pippo.tantan@gmail.com>
2018-08-13 16:55:42 +0800

    continue on ui

--- resources/views/layouts/partials/nav.blade.php
+++ resources/views/layouts/partials/nav.blade.php
@@ -1,18 +1,24 @@
 
       
-      <nav class="navbar navbar-expand-xl navbar-light bg-white">
-      <a class="navbar-brand" href="#" style="color: red;">LOGO</a>
-      <h5 class="my-0 mr-md-auto font-weight-normal">STA. FE STAND-ALONE SHS</h5>
-        <ul class="navbar-nav">
-            <li class="nav-item"><a class="nav-link" href="#">Students</a></li>
-            <li class="nav-item"><a class="nav-link" href="#">Parents</a></li>
-            <li class="nav-item"><a class="nav-link" href="#">Teachers</a></li>
-        </ul>
-        <form class="form-inline my-2 my-lg-0">
-            <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
-            <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-        </form>
-      </nav>
+    <nav class="navbar navbar-expand-xl navbar-light bg-white">
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarHead" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+    
+        <div class="collapse navbar-collapse" id="navbarHead">
+            <a class="navbar-brand" href="#" style="color: red;">LOGO</a>
+            <h5 class="my-0 mr-md-auto font-weight-normal">STA. FE STAND-ALONE SHS</h5>
+            <ul class="navbar-nav">
+                <li class="nav-item"><a class="nav-link" href="#">Students</a></li>
+                <li class="nav-item"><a class="nav-link" href="#">Parents</a></li>
+                <li class="nav-item"><a class="nav-link" href="#">Teachers</a></li>
+            </ul>
+            <form class="form-inline my-2 my-lg-0">
+                <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+                <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+            </form>
+        </div>
+    </nav>
       
 
 
