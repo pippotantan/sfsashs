dc3d74827030e5083547e751d06c19b2369a7f24
pippo-tantan <pippo.tantan@gmail.com>
2018-09-09 17:53:21 +0800

    accept changes on students

--- app/Student.php
+++ app/Student.php
@@ -15,13 +15,10 @@ class Student extends Model
         return $this->hasOne('App\User');
     }
 
-<<<<<<< HEAD
-=======
     public function klases(){
         return $this->hasMany('App\User');
     }
 
 
 
->>>>>>> b2ea6dbecce7924d186d81939849927a6259fc65
 }
